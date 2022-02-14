96530c8c9ce4779fd96cd4d6a2e142e16b495e2a
JJakDDo <kangdw124@gmail.com>
2022-02-14 22:16:59 +0900

    ADD: 주석 추가

--- 01-Expanding-Cards/script.js
+++ 01-Expanding-Cards/script.js
@@ -2,11 +2,17 @@ const cards = document.querySelectorAll('.card');
 const descs = document.querySelectorAll('.desc_container');
 
 const handleClick = (e) => {
+  // 클릭한 엘리멘트가 card 인지 확인
   if (e.target.classList.contains('card')) {
+    // 모든 desc_container 엘리멘트에 hide 클래스를 주기
     descs.forEach((desc) => desc.classList.add('hide'));
+    // 모든 card 엘리멘트에서 expand를 삭제하기
     cards.forEach((card) => card.classList.remove('expand'));
+
+    // 클릭한 엘리멘트에만 expand 클래스를 추가하기
     e.target.classList.add('expand');
-    console.log(e.target.children[0]);
+
+    // 카드가 다 펼쳐진 뒤에 (0.7초) desc_container에서 hide 클래스를 삭제해서 desc_container를 보여주기
     setTimeout(() => {
       e.target.children[0].classList.remove('hide');
     }, 700);
