a627ad1b23275b683fb416ddbd001c381f8ac03d
JJakDDo <kangdw124@gmail.com>
2022-08-13 12:08:12 +0900

    feat: check if the user type correct words and remove it

--- 13-TypingGame/script.js
+++ 13-TypingGame/script.js
@@ -1,10 +1,43 @@
 const wordsBox = document.querySelector(".words-list");
-const { x, y, width, height: boxHeight } = wordsBox.getBoundingClientRect();
+const input = document.querySelector(".word-input");
+let boxWidth, boxHeight;
+let words = [
+  "apple",
+  "banana",
+  "car",
+  "dive",
+  "elephant",
+  "fox",
+  "giraffe",
+  "hope",
+  "ink",
+  "jump",
+  "kite",
+  "lion",
+  "mule",
+  "neon",
+  "opportunity",
+];
+
+let currentWords = [];
+
+const getWidthAndHeight = () => {
+  boxWidth = wordsBox.getBoundingClientRect().width;
+  boxHeight = wordsBox.getBoundingClientRect().height;
+};
+
+getWidthAndHeight();
+
+const getNewWord = () => {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  currentWords.push(words[randomIndex]);
+  return words[randomIndex];
+};
 
 const intervalId = setInterval(() => {
   const word = document.createElement("div");
-  const left = Math.random() * (width - 100) + "px";
-  word.innerText = "hello";
+  const left = Math.random() * (boxWidth - 100) + "px";
+  word.innerText = getNewWord();
   word.classList.add("word");
   word.style.cssText = `position:absolute; top: 0; left: ${left}; width: 100px; text-align:center;color:#eaeaea;`;
   wordsBox.appendChild(word);
@@ -30,3 +63,18 @@ const getMovementByFrame = () => {
 };
 
 requestAnimationFrame(animation);
+
+input.addEventListener("input", () => {
+  let index = currentWords.indexOf(input.value);
+  if (index >= 0) {
+    const wordList = document.querySelectorAll(".word");
+    currentWords = [
+      ...currentWords.slice(0, index),
+      ...currentWords.slice(index + 1),
+    ];
+    wordsBox.removeChild(wordList[index]);
+    input.value = "";
+  }
+});
+
+window.addEventListener("resize", getWidthAndHeight);
